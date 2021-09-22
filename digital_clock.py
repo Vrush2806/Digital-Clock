42cf91b4efb579b237fe8d3061a38e327fcd3eca
Vrush2806 <68546263+Vrush2806@users.noreply.github.com>
2021-09-22 19:15:59 +0530

    Add files via upload

---
+++ digital_clock.py
@@ -0,0 +1,22 @@
+from tkinter import Label,Tk
+import time
+app_window=Tk()
+app_window.title("DIGITAL CLOCK")
+app_window.geometry("420x150")
+app_window.resizable(1,1)  #for fixed window size
+
+text_font=("Calibri",68,'bold')
+background="blue"
+foreground="orange"
+border_width=30
+
+label=Label(app_window,font=text_font,bg=background,fg=foreground,bd=border_width)
+label.grid(row=0,column=1)
+
+def clock():
+    time_live=time.strftime("%H:%M:%S")  #strftime() returns string of date-time
+    label.config(text=time_live)
+    label.after(200,clock)   #time in milli seconds & function which shall be called
+
+clock()
+app_window.mainloop()
